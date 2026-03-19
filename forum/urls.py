0a9e2b2ec16426f20eedacb57142e5bb4962a5ef
Kamila Matusik <kamilav7312@gmail.com>
2026-03-19 16:13:00 +0100

    Restructure django project

--- forum/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views # from current folder import the neighbour file views for views methods usage
-
-urlpatterns = [
-    path('', views.index),
-    path('frequent_questions', views.frequent_questions)
-]
- 
